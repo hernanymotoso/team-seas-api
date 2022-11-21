4099e9b13d0fc392c59ee8f6c393b7c29589770f
Hernany Motoso <contatohernany@gmail.com>
2022-11-20 21:48:35 -0300

    feat: generate donations resource, and config `ApolloDriver`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      playgroind: false,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
